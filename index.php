973fcfdf3998618f4886b8885b3e8c1893e0b7ad
Munsterberg <jetrussell93@gmail.com>
2016-01-14 19:31:47 -0500

    Bootstrap added, partials created

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once 'partials/head.php' ?>
+<?php include_once 'partials/header.php' ?>
+
+<div class="container">
+  <h1>Hi</h1>
+</div>
+
+<?php include_once 'partials/footer.php' ?>
