6d28a74ebf3a18625a5435bb59c0e8f7a40d530f
Munsterberg <jetrussell93@gmail.com>
2016-01-29 02:49:10 -0500

    Images added, slight design changes to hero section

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-default">
-  <div class="container">
+  <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNav" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -7,12 +7,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a href="/" class="navbar-brand">Logo</a>
     </div>
     <div class="collapse navbar-collapse" id="myNav">
       <ul class="nav navbar-nav">
         <li><a href="#">Contact</a></li>
         <li><a href="#">Portfolio</a></li>
+        <li><a href="/" class="navbar-brand logo"><img src="../img/logo.png" alt=""></a><li>
         <li><a href="#">About</a></li>
         <li><a href="#">Volunteer</a></li>
       </ul>
