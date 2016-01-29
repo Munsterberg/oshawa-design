b3c8a6628ad63c9afed83445905a3ce12659f856
Munsterberg <jetrussell93@gmail.com>
2016-01-29 01:51:54 -0500

    Index markup completed

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
 <?php include_once 'partials/head.php' ?>
 <?php include_once 'partials/header.php' ?>
 
-<div class="container">
-  <h1>Hi</h1>
+
+<div class="jumbotron landing-header">
+  <div class="container">
+    <h1 class="landing-title">Oshawa Design</h1>
+  </div>
 </div>
+
+<div class="cloud-section">
+  <div class="container">
+    <h2>Grow Together</h2>
+    <p>"Teamwork is the ability to work together toward a common vision. The ability to direct individual accomplishments toward organizational objectives. It is the fuel that allows common people to attain uncommon results." - Andrew Carnegie</p>
+  </div>
+</div>
+
+<div class="info-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h4>Website Development</h4>
+      </div>
+      <div class="col-md-4">
+        <h4>App Development</h4>
+      </div>
+      <div class="col-md-4">
+        <h4>Search Engine Optimization</h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="notice-section">
+  <div class="container">
+    <h2>Catchy Title</h2>
+    <button>CTA</button>
+  </div>
+</div>
+
+<div class="project-section">
+  <div class="container">
+    <h3>Responsive Websites</h3>
+    <p>Some small text under a title.</p>
+  </div>
+</div>
+
+
 
 <?php include_once 'partials/footer.php' ?>
