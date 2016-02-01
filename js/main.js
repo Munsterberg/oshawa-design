1eca86cbca5d98cbc9c2e9314524b0ac3252e251
Munsterberg <jetrussell93@gmail.com>
2016-01-31 22:08:25 -0500

    Show nav on scroll function and class added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function() {
+  $(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 130) {
+        $('.navbar').addClass('show-nav');
+        $('.logo').hide();
+      } else {
+        $('.navbar').removeClass('show-nav');
+        $('.logo').show();
+      }
+    });
+  });
+});
